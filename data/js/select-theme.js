3a585df4877b2529eb71e85df34bd068db82e273
erick <erick@localhost.localdomain>
2024-11-18 10:34:31 -0300

    Alterando tema padrão de inicializao #2

--- data/js/select-theme.js
+++ data/js/select-theme.js
@@ -1,31 +1,20 @@
-function detectTheme() {
-    const darkThemeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
 
-    if (darkThemeMediaQuery.matches) {
-        console.log('Tema atual: Escuro');
-        // Adicione sua lógica para o tema escuro aqui
+    // Carregar o tema salvo no localStorage
+    const darkMode = sessionStorage.getItem('dark-mode') === 'enabled';
+
+    // Aplica a classe 'dark-mode' com base na preferência armazenada
+    if (darkMode) {
+        wrapper.classList.add('dark-mode');
     } else {
-        console.log('Tema atual: Claro');
-        // Adicione sua lógica para o tema claro aqui
+        wrapper.classList.remove('dark-mode');
     }
 
-// Adicionar listener para alterações de tema
-    darkThemeMediaQuery.addEventListener('change', (event) => {
-        if (event.matches) {
-                console.log('Mudou para tema escuro');
-            const body = document.body;
-            const darkMode = localStorage.getItem('dark-mode') === 'enabled';
+    // Alterna o tema e salva a preferência
+    document.getElementById('theme-toggle').addEventListener('click', () => {
         wrapper.classList.toggle('dark-mode');
-        // Atualize a lógica para o tema escuro aqui
-        } else {
-            console.log('Mudou para tema claro');
-            const body = document.body;
-            const darkMode = localStorage.getItem('dark-mode') === 'disabled';
-        wrapper.classList.toggle('dark-mode');
-        // Atualize a lógica para o tema claro aqui
-        }
+        sessionStorage.setItem('dark-mode', wrapper.classList.contains('dark-mode') ? 'enabled' : 'disabled');
     });
-}
+});
 
-// Chame a função ao carregar a aplicação
-detectTheme();
